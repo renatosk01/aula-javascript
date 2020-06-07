ac821b5287d2d52f9063f5477c9ca9906ab50d1a
Renato Morais <renato.s.morais@hotmail.com>
2020-06-07 17:30:01 -0300

    Javascript teste

---
+++ js/main.js
@@ -0,0 +1,98 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar </b>";
+   // console.log(document.getElementById("agradecimento"));
+   // alert("Obrigado por clicar ")
+
+
+}
+function redi(){
+    window.open("https://sulamylobato34.wixsite.com/nutri");
+}
+
+function trocar(){
+    alert("trocar texto");
+
+}
+
+
+/*
+function soma(n1,n2){
+    return n1 +n2
+}
+
+alert(soma (5,10));
+
+*/
+
+
+/*
+function validaidade(idade){
+    var validar;
+    if (idade >= 18){
+        validaidadelidar = true;
+    }else{
+        validar = false;
+    }
+    return validar;
+}
+
+
+var idade = prompt("Qual sua idade:");
+console.log(validaidade(idade));
+
+*/
+
+
+/*
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+alert(setReplace("Vai japao " , "japao", "Brasil"));
+*/
+
+/*
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours));
+
+
+
+/*
+
+var idade = prompt("Qual sua idade");
+//var idade =18;
+if(idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+};
+
+*/
+
+
+
+
+
+/*
+var lista = ["maca", " pera", "laranja"] ;
+lista.push("uva");
+
+console.log(lista);
+*/
+
+
+
+
+/*
+var nome = "Renato Morais";
+var idade = 29;
+var idade2 = 10;
+var frase = " japao e o melhor timne do mundo ";
+//alert(nome + " tem " + idade + " anos");
+//alert( idade + idade2);
+console.log(nome);
+//console.log(frase.replace("japao", "Brasil"));
+
+console.log(frase.toLocaleLowerCase());
+*/
